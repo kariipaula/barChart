a60efef521bc486f221a5928e964d6b7405fcc0b
Italo Boss <italoboss@gmail.com>
2020-02-24 17:29:30 -0300

    start project

---
+++ barChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  barChart
+//
+//  Created by Italo Boss on 24/02/20.
+//  Copyright © 2020 Italo Boss. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
