a5fb14e2774f7f054f03b61e20da0c4499222b9d
Karina Paula <kariipaula@gmail.com>
2020-02-24 22:27:37 -0300

    add structure and filter data

---
+++ barChart/ChartInfo.swift
@@ -0,0 +1,16 @@
+//
+//  ChartInfo.swift
+//  barChart
+//
+//  Created by Karina Paula on 24/02/2020.
+//  Copyright © 2020 Italo Boss. All rights reserved.
+//
+
+import Foundation
+
+struct ChartInfo {
+    
+    var value: Float
+    var date: Int
+    
+}
--- barChart/ContentView.swift
+++ barChart/ContentView.swift
@@ -9,21 +9,54 @@
 import SwiftUI
 
 struct BarChart: View {
+    
+    var dataSet : [ChartInfo] = [
+        ChartInfo(value: 1.0, date: 2019),
+        ChartInfo(value: 2.0, date: 2019),
+        ChartInfo(value: 3.0, date: 2018),
+        ChartInfo(value: 4.0, date: 2018),
+        ChartInfo(value: 5.0, date: 2019),
+        ChartInfo(value: 6.0, date: 2019),
+        ChartInfo(value: 7.0, date: 2019),
+        ChartInfo(value: 8.0, date: 2018),
+        ChartInfo(value: 9.0, date: 2018),
+        ChartInfo(value: 10.0, date: 2018),
+        ChartInfo(value: 11.0, date: 2019)
+    ]
+    
+    func sumValues(_ year: Int) -> Float {
+        let filterResult = self.dataSet.filter { (recordChartInfo) -> Bool in
+            recordChartInfo.date == year
+        }
+        
+        let reduceResult = filterResult.reduce(0.0) { (result, recordChartInfo) -> Float in
+            return result+recordChartInfo.value
+        }
+        
+        return reduceResult
+    }
+
+    
     var body: some View {
         VStack {
-            VStack(alignment: .leading) {
-                Text("Primeira atividade")
-                    .foregroundColor(Color(red: 0.98, green: 0.46, blue: 0.41, opacity: 1.0))
-                    .kerning(1.2)
-                    .fontWeight(.light)
-                    .font(.largeTitle)
-                
-                Text("Gráfico de barras")
-                    .italic()
-                    .font(.body)
-                    .fontWeight(.light)
-                    .foregroundColor(Color.gray)
+            HStack {
+                VStack(alignment: .leading) {
                     
+                    Text("Primeira atividade")
+                        .foregroundColor(Color(red: 0.98, green: 0.46, blue: 0.41, opacity: 1.0))
+                        .kerning(1.2)
+                        .fontWeight(.light)
+                        .font(.largeTitle)
+                    
+                    Text("Gráfico de barras")
+                        .italic()
+                        .font(.body)
+                        .fontWeight(.light)
+                        .foregroundColor(Color.gray)
+                        
+                }
+                .padding(20)
+                Spacer()
             }
             
             Spacer()
@@ -46,6 +79,8 @@ struct BarChart: View {
                     }
                 )
             }
+            Spacer()
+
             
         }
         
